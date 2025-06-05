211e15be1276e15c85b30fe0bb46c695a6b23858
unknown <rafifrezhaa@gmail.com>
2025-06-05 13:44:21 +0700

    123220041 Projek Akhir

--- frontend/script.js
+++
@@ -1,1 +0,0 @@
-const API_BASE_URL = "https://rmjrentcar-86067911510.asia-southeast2.run.app";
--- frontend/src/utils.js
+++
@@ -1,2 +0,0 @@
-
-export const API_BASE_URL = "https://rmjrentcar-86067911510.asia-southeast2.run.app";
